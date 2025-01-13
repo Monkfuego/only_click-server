b301d1cde63d722b942d0306a6eac74a39287818
vivekkedia22 <vivekkedia22@gmail.com>
2025-01-13 23:45:05 +0530

    category wise services

--- server/graphql/src/controllers/service.controller.js
+++ server/graphql/src/controllers/service.controller.js
@@ -12,6 +12,22 @@ const getAllServices = async (req, res) => {
 };
 const getCategoryService = async (req, res) => {
   const { category } = req.params;
-  
+  console.log("this is category", category);
+  console.log(await Service.find({ category: { $in: [`${category}`] } }));
+
+  try {
+    const services = await Service.find({ category: { $in: [`${category}`] } });
+    console.log("this is services", services);
+
+    return res.status(200).json({
+      message: "Fetched services accrding to category successfully",
+      services,
+    });
+  } catch (error) {
+    console.log("error while getting services for category", error);
+    return res
+      .status(400)
+      .json({ error: "error while getting services for category" });
+  }
 };
 export { getAllServices, getCategoryService };
